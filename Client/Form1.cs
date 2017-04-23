f6fb90b2501942787dc2f4661f1273512daffdba
erith <erith9@gmail.com>
2017-04-23 14:00:31 +0900

    초기 커밋.

---
+++ Client/Form1.cs
@@ -0,0 +1,50 @@
+﻿using PoviMartLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Thrift.Protocol;
+using Thrift.Transport;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+
+        TTransport transport;
+        MarketService.Client client;
+
+        /// <summary>
+        /// 사용이 유효한지 확인
+        /// </summary>
+        bool IsAvaliable
+        {
+            get
+            {
+                return transport != null && transport.IsOpen;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            transport = new TSocket("192.168.0.70", 9090);
+            TProtocol protocol = new TBinaryProtocol(transport);
+            client = new MarketService.Client(protocol);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            transport.Open();
+            var rows = client.GetProductList(2000);
+            dataGridView1.DataSource = rows;
+            transport.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,82 @@
+﻿using PoviMartLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thrift.Server;
+using Thrift.Transport;
+
+namespace Server
+{
+    public class ServerHandler : MarketService.Iface
+    {
+        public ResultCode DeleteProduct(int Number)
+        {
+            if (Number == 1)
+            {
+                return new ResultCode() { CODE = -1, MESSAGE = "처리실패", DESCRIPTION = "권한이 없는 상품입니다." };
+            }
+            else
+            {
+                return new ResultCode() { CODE = 0, MESSAGE = "처리완료", DESCRIPTION = "처리가 완료되었습니다." };
+            }
+        }
+
+        public List<Product> GetProductList(int limit)
+        {
+            Random r = new Random();
+            var rows = new List<Product>();
+
+            for(var i=0;i<limit;i++)
+            {                
+                rows.Add(new Product()
+                {
+                    NUMBER = i+1,
+                    NAME = $"상품_{i}",
+                    COST = r.Next(0, 200000),
+                    REGDATE = DateTime.Now.Ticks
+                });
+            }
+            return rows;
+        }
+
+        public ResultCode InsertProduct(string Name, string Email, long RegDate)
+        {
+            //new DateTime(RegDate);
+
+            return new ResultCode() { CODE = 0, MESSAGE = "처리완료", DESCRIPTION = $"{Name}, {Email}입력이 완료되었습니다." };
+        }
+
+        public void SendMail(Customer customer, SendType sendtype)
+        {
+            //TODO MAIL SENDING
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServerHandler handler = new ServerHandler();
+                MarketService.Processor processor = new MarketService.Processor(handler);
+                TServerTransport serverTransport = new TServerSocket(9090);
+                TServer server = new TSimpleServer(processor, serverTransport);
+
+                // Use this for a multithreaded server
+                // server = new TThreadPoolServer(processor, serverTransport);
+
+                Console.WriteLine("Starting the server...");
+                server.Serve();
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.StackTrace);
+            }
+            Console.WriteLine("done.");
+        }
+    }
+}
